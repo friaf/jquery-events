ec0aeb3393e1236ebe35e827489722f8b78d4797
friaf <shovel.irena@gmail.com>
2023-02-02 15:30:44 +0000

    last

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+    $("#stream1_btn").click(function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
@@ -17,4 +17,19 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
    });
+   $("#stream1_btn").on(function() {
+    $(".stream1").toggle(1000);
+    $(".stream2").slideDown('slow');
+    $(".stream3").show('medium');
+    $(".stream1").hide('fast');
+    $(".stream1").slideUp('fast');
+    $(".stream1").fadeIn('fast');
+    $(".stream1").fadeOut('fast');
+    $(".stream1").fadeToggle('fast');
+    $(".stream1").fadeTo(1000, 0,5);
+
 }); 
+
+$('#myButton').removeClass('blueBox').addClass('border');
+$('p').css('color','blue').slideUp(2000).slideDown(2000);
+$('a').attr('href','http://www.example.com');
